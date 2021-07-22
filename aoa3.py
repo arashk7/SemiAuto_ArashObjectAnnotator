8d539a75fff0ba095636dc080a2f8f46212249ab
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2021-07-22 17:03:10 +0800

    update

--- aoa3.py
+++ aoa3.py
@@ -8,6 +8,7 @@ import tkinter as tk
 from tkinter import filedialog
 import glob
 import SessionState as SS
+import re
 
 stroke_width = 3
 stroke_color = "#000000"
@@ -20,9 +21,8 @@ root.withdraw()
 # Make folder picker dialog appear on top of other windows
 root.wm_attributes('-topmost', 1)
 
-btn_dbdir = st.button('Select new dataset directory')
-if btn_dbdir:
-    dir_db = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
+st.write('Output directory is D:/dataset/out_db/train')
+out_dir = 'D:/dataset/out_db/train'
 
 btn_inputdir = st.button('Select input image directory')
 ss = SS.get(file_list='', counter=0, output_dir='', clear=False)
@@ -40,7 +40,7 @@ bg_image = cv2.imread(ss.file_list[ss.counter])
 basename = os.path.basename(os.path.normpath(ss.file_list[ss.counter]))
 
 file_name = basename.split('.')[0]
-print(file_name)
+# print(file_name)
 
 # '''Load a json file contains bounding boxes'''
 # json_all = {'version': '4.4.0', 'objects': [], 'background': '#eee'}
@@ -65,12 +65,27 @@ else:
 
         # json_all['objects'] = None
 
-print(json_all)
-fill_color= "rgba(255, 0, 0, 0.3)"
+# print(json_all)
+
 if btn_class=='person':
+    fill_color = "rgba(255, 0, 0, 0.3)"
+else:
     fill_color = "rgba(0, 255, 0, 0.3)"
 def color_to_class(color_str):
-    
+    out = 'test'
+    str = re.findall(r'[\d\.\d]+',color_str)
+    print(str)
+    if int(str[0])==255 and int(str[1])==0 and int(str[2])==0:
+        out = 'person'
+    elif int(str[0]) == 0 and int(str[1]) == 255 and int(str[2]) == 0:
+        out = 'forklift'
+    elif int(str[0]) == 0 and int(str[1]) == 0 and int(str[2]) == 255:
+        out = 'box'
+    return out
+def class_to_id(class_str):
+    classes=['person','forklift']
+    return classes.index(class_str)
+# print(color_to_class(fill_color))
 
 canvas_result = st_canvas(
     fill_color=fill_color,  # Fixed fill color with some opacity
@@ -86,29 +101,53 @@ canvas_result = st_canvas(
     key=0
 )
 
-
+from shutil import copyfile
 def write_json():
     json_dict = canvas_result.json_data["objects"]
     jsonString = json.dumps(json_dict)
     jsonFile = open('json/' + file_name + '.json', "w")
     jsonFile.write(jsonString)
 
+def copy_img():
+    src = ss.file_list[ss.counter]
+    dst = out_dir+'/images/'+basename
+    copyfile(src,dst)
+
+def write_label_txt():
+    dst = out_dir + '/labels/' + file_name+'.txt'
+    file_txt = open(dst,'w')
+    dict_obj=canvas_result.json_data["objects"]
+
+    for key in dict_obj:
+        print(key['fill'])
+        cls_id=class_to_id(color_to_class(key['fill']))
+        x=key['left']
+        y=key['top']
+        w=key['width']
+        h=key['height']
+        L=str(cls_id)+' '+str(x)+' '+str(y)+' '+str(w)+' '+str(h)+'\n'
+        file_txt.writelines(L)
+    file_txt.close()
 
 col1, col2 = st.beta_columns(2)
-btn_next = col1.button('Next')
-if btn_next:
-    write_json()
-    ss.counter += 1
-    canvas_result.json_data["objects"] = None
-    ss.clear = True
-    st.experimental_rerun()
-
-
-btn_prev = col2.button('Prev')
+btn_prev = col1.button('Save & Prev')
 if btn_prev:
     write_json()
+    copy_img()
+    write_label_txt()
     ss.counter -= 1
     canvas_result.json_data["objects"] = None
-    ss.clear = True
     st.experimental_rerun()
+
+btn_next = col2.button('Save & Next')
+if btn_next:
+    write_json()
+    copy_img()
+    write_label_txt()
+    ss.counter += 1
+    canvas_result.json_data["objects"] = None
+    st.experimental_rerun()
+
+
+
 
